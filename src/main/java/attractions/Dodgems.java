bcc25e8d9e1d754bb206f1c7549da8f04958e412
Jonny Reading <jonnyreading@gmail.com>
2020-09-28 17:11:37 +0100

    dodgems lets kids on half price

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -1,8 +1,26 @@
 package attractions;
 
+import people.Visitor;
+
 public class Dodgems extends Attraction {
+
+    private double price;
 
     public Dodgems(String name, int rating) {
         super(name, rating);
+        this.price = 4.50;
     }
+
+    public double defaultPrice() {
+        return this.price;
+    }
+
+    public double priceFor(Visitor visitor) {
+        double multiplier = 1;
+        if (visitor.getAge() < 12) {
+            multiplier -= 0.5;
+        }
+        return defaultPrice() * multiplier;
+    }
+
 }
--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,6 +2,7 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
@@ -28,5 +29,17 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void hasPrice() {
+        Visitor visitor = new Visitor(20, 1.50, 30.00);
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.01);
+    }
+
+    @Test
+    public void kidsGoOnHalfPrice() {
+        Visitor visitor = new Visitor(10, 1.50, 30.00);
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.01);
     }
 }
